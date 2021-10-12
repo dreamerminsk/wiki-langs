649d775ace74e4442e5daa2d629b54e1d5a4519f
Micah Teeuws <tutmirage@gmail.com>
2021-10-12 21:38:00 +0300

    Update fizzbuzz.rs

--- src/fizzbuzz.rs
+++ src/fizzbuzz.rs
@@ -27,3 +27,16 @@ where
 {
     type Sum = SumOf<N, S<M>>;
 }
+
+
+
+type One = S<Z>;
+type Two = SumOf<One, One>;
+type Three = SumOf<One, Two>;
+type Five = SumOf<Two, Three>;
+type Ten = SumOf<Five, Five>;
+type TwentyFive = SumOf<Five, SumOf<Ten, Ten>>;
+type Fifty = SumOf<TwentyFive, TwentyFive>;
+type OneHundred = SumOf<Fifty, Fifty>;
+
+type N = OneHundred;
