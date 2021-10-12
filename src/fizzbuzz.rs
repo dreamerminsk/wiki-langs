6c699d6709b1b8a1cae7ef9d3d516195ff5de68c
Micah Teeuws <tutmirage@gmail.com>
2021-10-12 21:36:51 +0300

    Update fizzbuzz.rs

--- src/fizzbuzz.rs
+++ src/fizzbuzz.rs
@@ -15,3 +15,15 @@ trait Add<Rhs> {
 }
 
 type SumOf<N, M>     =     <N as Add<M>>::Sum;
+
+
+impl<N> Add<N> for Z {
+    type Sum = N;
+}
+
+impl<N, M> Add<M> for S<N>
+where
+    N: Add<S<M>>,
+{
+    type Sum = SumOf<N, S<M>>;
+}
