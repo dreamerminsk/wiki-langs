4920e1744dfc1fc08aefa600e270b05c6ca15116
Micah Teeuws <tutmirage@gmail.com>
2022-05-13 22:54:07 +0300

    Update main.rs
    const SEEDINGS;

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 extern crate reqwest;
 extern crate select;
 
+
+
+const SEEDINGS  :  &str   =  "http://www.snooker.org/res/index.asp?template=32&season=2021" ; 
+
+
+
+
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
     let content = reqwest::get("http://httpbin.org/range/26")
