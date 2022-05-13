1835be159eefbe12710040a70a51712a74966930
Micah Teeuws <tutmirage@gmail.com>
2022-05-13 20:32:02 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 extern crate reqwest;
 extern crate select;
-
+#[tokio:main]
 fn main() {
+    let content = reqwest::get("http://httpbin.org/range/26")
+    .await?
+    .text()
+    .await?;
     println!("Hello, world!");
 }
