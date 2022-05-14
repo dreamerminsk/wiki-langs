8b4b26fbabc8b6419768d263c3f7136a9afbcc31
Micah Teeuws <tutmirage@gmail.com>
2022-05-14 22:12:27 +0300

    Update snooker.rs

--- src/snooker.rs
+++ src/snooker.rs
@@ -1,9 +1,9 @@
-const RESULTS: &str = "http://www.snooker.org/res/index.asp?template=22&season=2021";
-const UPCOMINGMATCHES: &str = "http://www.snooker.org/res/index.asp?template=24";
+pub const RESULTS: &str = "http://www.snooker.org/res/index.asp?template=22&season=2021";
+pub const UPCOMINGMATCHES: &str = "http://www.snooker.org/res/index.asp?template=24";
 
-const RANKINGS: &str = "http://www.snooker.org/res/index.asp?template=31&season=2021";
-const SEEDINGS: &str = "http://www.snooker.org/res/index.asp?template=32&season=2021";
-const SEASONPOINTS: &str = "http://www.snooker.org/res/index.asp?template=33&season=2021";
+pub const RANKINGS: &str = "http://www.snooker.org/res/index.asp?template=31&season=2021";
+pub const SEEDINGS: &str = "http://www.snooker.org/res/index.asp?template=32&season=2021";
+pub const SEASONPOINTS: &str = "http://www.snooker.org/res/index.asp?template=33&season=2021";
 
-const PLAYER: &str = "http://www.snooker.org/res/index.asp?player=";
-const EVENT: &str = "http://www.snooker.org/res/index.asp?event=";
+pub const PLAYER: &str = "http://www.snooker.org/res/index.asp?player=";
+pub const EVENT: &str = "http://www.snooker.org/res/index.asp?event=";
