f6bfcd88e1028bf5cdf994291f6e65e993e02bfa
Micah Teeuws <tutmirage@gmail.com>
2022-05-15 12:18:50 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,15 @@ extern crate select;
 
 mod snooker;
 
+use scraper::{Html, Selector};
+use std::collections::HashSet;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let resp = reqwest::get("https://www.wireshark.org/download/").await?;
+    println!("{}", resp.url().to_string());
+
+
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
     let content = reqwest::get(snooker::UPCOMINGMATCHES).await?.text().await?;
