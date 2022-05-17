fb2fa8813de83827d43af288f8e7912a239f4bae
Micah Teeuws <tutmirage@gmail.com>
2022-05-17 10:17:14 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,8 @@ mod snooker;
 use scraper::{Html, Selector};
 use std::collections::HashSet;
 
-fn parse_links(text: &str) -> HashSet<String> {}
-
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let resp = reqwest::get(snooker::UPCOMINGMATCHES).await?;
-    println!("{}", resp.url().to_string());
-
-    let text = resp.text().await?;
-
-    let mut urls: HashSet<String> = HashSet::new();
+fn parse_links(text: &str) -> HashSet<String> {
+   let mut urls: HashSet<String> = HashSet::new();
     let document = Html::parse_document(&text);
     let selector = Selector::parse(r#"a"#).unwrap();
     for title in document.select(&selector) {
@@ -28,6 +20,17 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             urls.insert(url);
         }
     }
+urls;
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let resp = reqwest::get(snooker::UPCOMINGMATCHES).await?;
+    println!("{}", resp.url().to_string());
+
+    let text = resp.text().await?;
+
+    let mut urls: HashSet<String> = parse_links(&text);
 
     for url in urls {
         println!("{}", url);
