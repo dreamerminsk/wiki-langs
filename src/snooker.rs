4ed7ea7ff0a168129dc3b0c47d0cc4377d451acd
Micah Teeuws <tutmirage@gmail.com>
2022-06-04 12:47:30 +0300

    Update snooker.rs

--- src/snooker.rs
+++ src/snooker.rs
@@ -14,7 +14,7 @@ pub const RANKINGS: &str = "/res/index.asp?template=31&season={}";
 pub const SEEDINGS: &str = "/res/index.asp?template=32&season={}";
 pub const POINTS: &str = "/res/index.asp?template=33&season={}";
 
-pub const PLAYER: &str = "/res/index.asp?player={}";
+pub const PLAYER: &str = "/res/index.asp?player=";
 pub const EVENT: &str = "/res/index.asp?event={}";
 
 fn query(u: Url) -> HashMap<String, String> {
