72070065d2e567f23dbc5b1a3c884c1bceb4c680
Micah Teeuws <tutmirage@gmail.com>
2022-06-08 11:50:20 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,8 @@ fn parse_links(text: &str) -> BTreeSet<Link> {
 }
 
 fn add_player(plink: &PlayerLink) -> Result<(), Box<dyn Error>> {
-    fs::create_dir_all("./events/")?;
+    fs::create_dir_all("./players/")?;
+let source_file = format!("./players/ids.{:0>4}.csv", (plink.snooker_id).to_string());
     Ok(())
 }
 
