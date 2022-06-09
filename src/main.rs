f0fe7ce1cb9e0477f3932f2fe997ff767146215b
Micah Teeuws <tutmirage@gmail.com>
2022-06-09 19:46:35 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,17 @@ use std::error::Error;
 use std::fs;
 use std::path::Path;
 use uuid::Uuid;
+use reqwest::Client;
 
 mod html;
 mod snooker;
+
+
+
+
+
+
+static APP_USER_AGENT : &str = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36 Edg/102.0.1245.33";
 
 fn add_player(plink: &PlayerLink) -> Result<(), Box<dyn Error>> {
     fs::create_dir_all("./players/")?;
@@ -64,13 +72,12 @@ fn add_event(elink: &EventLink) -> Result<(), Box<dyn Error>> {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let client = reqwest::Client::builder()
+    let client = Client::builder()
         .user_agent(APP_USER_AGENT)
         .build()?;
 
     let resp = client
         .get(format!("{}{}", snooker::HOST, snooker::UPCOMING_MATCHES))
-        .header(UserAgent::new("foo"))
         .send()
         .await?;
 
