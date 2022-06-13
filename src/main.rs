1be6296305529ba1c8d8465102fcc70580d4ff92
Micah Teeuws <tutmirage@gmail.com>
2022-06-13 18:47:02 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     let cur_url = match (now.hour() + now.minute()) % 5 {
         0 => format!("{}{}", snooker::upcoming_matches(), "&numperpage=50&page=5"),
-        1 => format!("{}{}", snooker::results(2019), "&numperpage=50&page=3"),
+        1 => format!("{}{}", snooker::results(2020), "&numperpage=50&page=4"),
         2 => snooker::rankings(2019),
         _ => "http://www.snooker.org/res/index.asp?template=2&season=2018".to_string(),
     };
