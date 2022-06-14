b26b3acfbf3bf834512f7ad00e47892a55220841
Micah Teeuws <tutmirage@gmail.com>
2022-06-14 19:32:38 +0300

    Update tables.rs

--- src/tables.rs
+++ src/tables.rs
@@ -12,41 +12,8 @@ pub struct PlayerTable {
 pub fn add_player(plink: &PlayerLink) -> Result<(), Box<dyn Error>> {
     fs::create_dir_all("./players/")?;
     let source_name = format!("./players/{}", get_id_segment(plink.snooker_id));
-    let temp_name = format!("./players/{}.csv", Uuid::new_v4());
     if Path::new(&source_name).exists() {
-        {
-            let mut source_reader = csv::Reader::from_path(&source_name)?;
-            let mut temp_writer = csv::Writer::from_path(&temp_name)?;
-            let mut saved = false;
-            for link in source_reader.deserialize() {
-                let link: PlayerLink = link?;
-                if saved {
-                    temp_writer.serialize(link)?;
-                } else {
-                    match link.cmp(plink) {
-                        Ordering::Greater => {
-                            temp_writer.serialize(plink)?;
-                            temp_writer.serialize(link)?;
-                            saved = true;
-                        }
-                        Ordering::Less => temp_writer.serialize(link)?,
-                        Ordering::Equal => {
-                            match link.full_name.len().cmp(&plink.full_name.len()) {
-                                Ordering::Greater => temp_writer.serialize(link)?,
-                                _ => temp_writer.serialize(plink)?,
-                            }
-                            saved = true;
-                        }
-                    }
-                }
-            }
-            if !saved {
-                temp_writer.serialize(plink)?;
-            }
-            temp_writer.flush()?;
-        }
-        fs::remove_file(&source_name)?;
-        fs::rename(&temp_name, &source_name)?;
+   update_segment(&source_name,&plink);
     } else {
         let mut source_writer = csv::Writer::from_path(&source_name)?;
         source_writer.serialize(plink)?;
