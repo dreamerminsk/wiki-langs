30effe62f5dbf4f4c6e80644310299efde9b5ce9
Micah Teeuws <tutmirage@gmail.com>
2022-06-14 19:39:08 +0300

    Update tables.rs

--- src/tables.rs
+++ src/tables.rs
@@ -62,41 +62,8 @@ fn update_segment(segment: &str, pl: &PlayerLink) -> Result<(), Box<dyn Error>> 
 pub fn add_event(elink: &EventLink) -> Result<(), Box<dyn Error>> {
     fs::create_dir_all("./events/")?;
     let source_name = format!("./events/{}", get_id_segment(elink.snooker_id));
-    let temp_name = format!("./events/{}.csv", Uuid::new_v4());
     if Path::new(&source_name).exists() {
-        {
-            let mut source_reader = csv::Reader::from_path(&source_name)?;
-            let mut temp_writer = csv::Writer::from_path(&temp_name)?;
-            let mut saved = false;
-            for link in source_reader.deserialize() {
-                let link: EventLink = link?;
-                if saved {
-                    temp_writer.serialize(link)?;
-                } else {
-                    match link.cmp(elink) {
-                        Ordering::Greater => {
-                            temp_writer.serialize(elink)?;
-                            temp_writer.serialize(link)?;
-                            saved = true;
-                        }
-                        Ordering::Less => temp_writer.serialize(link)?,
-                        Ordering::Equal => {
-                            match link.title.len().cmp(&elink.title.len()) {
-                                Ordering::Greater => temp_writer.serialize(link)?,
-                                _ => temp_writer.serialize(elink)?,
-                            }
-                            saved = true;
-                        }
-                    }
-                }
-            }
-            if !saved {
-                temp_writer.serialize(elink)?;
-            }
-            temp_writer.flush()?;
-        }
-        fs::remove_file(&source_name)?;
-        fs::rename(&temp_name, &source_name)?;
+    update_esegment(&source_name,&elink);
     } else {
         let mut source_writer = csv::Writer::from_path(&source_name)?;
         source_writer.serialize(elink)?;
@@ -104,6 +71,48 @@ pub fn add_event(elink: &EventLink) -> Result<(), Box<dyn Error>> {
     }
     Ok(())
 }
+
+
+
+
+fn update_esegment(segment: &str, el: &EventLink) -> Result<(), Box<dyn Error>> {
+    let temp_name = format!("./events/{}.csv", Uuid::new_v4());
+    {
+        let mut source_reader = csv::Reader::from_path(&segment)?;
+        let mut temp_writer = csv::Writer::from_path(&temp_name)?;
+        let mut saved = false;
+        for link in source_reader.deserialize() {
+            let link: EventLink = link?;
+            if saved {
+                temp_writer.serialize(link)?;
+            } else {
+                match link.cmp(pl) {
+                    Ordering::Greater => {
+                        temp_writer.serialize(el)?;
+                        temp_writer.serialize(link)?;
+                        saved = true;
+                    }
+                    Ordering::Less => temp_writer.serialize(link)?,
+                    Ordering::Equal => {
+                        match link.title.len().cmp(&el.title.len()) {
+                            Ordering::Greater => temp_writer.serialize(link)?,
+                            _ => temp_writer.serialize(el)?,
+                        }
+                        saved = true;
+                    }
+                }
+            }
+        }
+        if !saved {
+            temp_writer.serialize(el)?;
+        }
+        temp_writer.flush()?;
+    }
+    fs::remove_file(&segment)?;
+    fs::rename(&temp_name, &segment)?;
+}
+
+
 
 fn get_id_segment(id: u32) -> String {
     format!("ids.{:0>4}.csv", (100 * (id / 100) + 99).to_string())
