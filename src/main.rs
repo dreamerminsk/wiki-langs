a55d0007872df5dda3c5f0b61fa072d037a2171d
Micah Teeuws <tutmirage@gmail.com>
2022-07-01 19:17:42 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let random_id: u32 = rng.gen_range(1..300);
     let player = snooker::get_player(usize::try_from(random_id)?).await?;
     tables::add_player(&player)?;
+(0..3).map(|_| rng.gen_range(1..300)).filter_map(|r| usize::try_from(r).ok()).filter_map(|id| snooker::get_player(id).await.ok()).for_each(|p| tables::add_player(&p)?);
 
     Ok(())
 }
