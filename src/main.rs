fd06f5c8e75bd91a701369f54b925d29bf36e0c2
Micah Teeuws <tutmirage@gmail.com>
2022-07-01 19:53:14 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -9,15 +9,12 @@ mod tables;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let mut rng = rand::thread_rng();
+let mut rng = rand::thread_rng();
+for i in 1..4 {
     let random_id: u32 = rng.gen_range(1..300);
     let player = snooker::get_player(usize::try_from(random_id)?).await?;
     tables::add_player(&player)?;
-    (0..3)
-        .map(|_| rng.gen_range(1..300))
-        .filter_map(|r| usize::try_from(r).ok())
-        .filter_map(move |id| async move { snooker::get_player(id).await.unwrap().ok() })
-        .for_each(|p| {tables::add_player(&p);Ok(()));
+}
 
     Ok(())
 }
