6ddacd164c5d880a8a5c5ee9e4ce66cfa5eb277c
Micah Teeuws <tutmirage@gmail.com>
2022-07-02 22:58:17 +0300

    Update country.rs

--- src/domain/country.rs
+++ src/domain/country.rs
@@ -1,4 +1,6 @@
 use serde::{Deserialize, Serialize};
+use std::cmp::Ordering;
+use std::hash::{Hash,Hasher};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Country {
@@ -27,5 +29,20 @@ impl PartialOrd for Country {
 impl PartialEq for Country {
     fn eq(&self, other: &Self) -> bool {
         self.name == other.name
+    }
+}
+
+
+
+
+
+
+
+
+impl Eq for      Country     {}
+
+impl Hash for     Country      {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.name.hash(state);
     }
 }
