5d106764aa89457d9056720e86584c91082c3de5
Micah Teeuws <tutmirage@gmail.com>
2022-07-03 21:26:04 +0300

    Update entities.rs

--- src/domain/entities.rs
+++ src/domain/entities.rs
@@ -7,8 +7,8 @@ use std::hash::{Hash, Hasher};
 pub struct Country {
     pub name: String,
     pub iso_num: Option<String>,
-    pub iso_alpha_2: Option<String>,
-    pub iso_alpha_3: Option<String>,
+    pub iso_2: Option<String>,
+    pub iso_3: Option<String>,
     pub ioc_cc: Option<String>,
     pub fifa_cc: Option<String>,
     pub wiki_data_id: Option<String>,
