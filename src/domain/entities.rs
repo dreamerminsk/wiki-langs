f834400e39a0d8be40714ecc5c9a4fbe53345a39
Micah Teeuws <tutmirage@gmail.com>
2022-07-03 11:06:22 +0300

    Create entities.rs

---
+++ src/domain/entities.rs
@@ -0,0 +1,57 @@
+use serde::{Deserialize, Serialize};
+use std::cmp::Ordering;
+use std::convert::From;
+use std::hash::{Hash, Hasher};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Country {
+    pub name: String,
+    pub iso_num: Option<String>,
+    pub iso_alpha_2: Option<String>,
+    pub iso_alpha_3: Option<String>,
+    pub ioc_cc: Option<String>,
+    pub fifa_cc: Option<String>,
+    pub wiki_data_id: Option<String>,
+    pub wiki_id: Option<String>,
+}
+
+impl Ord for Country {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.name.cmp(&other.name)
+    }
+}
+
+impl PartialOrd for Country {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl PartialEq for Country {
+    fn eq(&self, other: &Self) -> bool {
+        self.name == other.name
+    }
+}
+
+impl Eq for Country {}
+
+impl Hash for Country {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.name.hash(state);
+    }
+}
+
+impl From<String> for Country {
+    fn from(value: String) -> Self {
+        Country {
+            name: value.to_owned(),
+            iso_num: None,
+            iso_alpha_2: None,
+            iso_alpha_3: None,
+            ioc_cc: None,
+            fifa_cc: None,
+            wiki_data_id: None,
+            wiki_id: None,
+        }
+    }
+}
