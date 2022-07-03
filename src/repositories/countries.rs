7d51d97c1c2a020fb143ac98450423cfa1bc8c63
Micah Teeuws <tutmirage@gmail.com>
2022-07-03 12:14:19 +0300

    Update countries.rs

--- src/repositories/countries.rs
+++ src/repositories/countries.rs
@@ -1,46 +1,55 @@
-pub fn add_player(player: &Player) -> Result<(), Box<dyn Error>> {
-    fs::create_dir_all("./players/")?;
-    let source_name = format!("./players/{}", get_year_segment(player.birthday));
+use crate::snooker::{EventLink, Player, PlayerLink};
+use chrono::{Datelike, NaiveDate};
+use std::cmp::Ordering;
+use std::error::Error;
+use std::fs;
+use std::path::Path;
+use uuid::Uuid;
+use crate::domain::entities::Country;
+
+
+
+
+pub fn add_country(country: &Country) -> Result<(), Box<dyn Error>> {
+    fs::create_dir_all("./countries/")?;
+    let source_name = format!("./countries/{}","names.eng.csv");
     if Path::new(&source_name).exists() {
-        update_player_segment(&source_name, player)?;
+        update_country_segment(&source_name, country)?;
     } else {
         let mut source_writer = csv::Writer::from_path(&source_name)?;
-        source_writer.serialize(player)?;
+        source_writer.serialize(country)?;
         source_writer.flush()?;
     }
     Ok(())
 }
 
-fn update_player_segment(segment: &str, player: &Player) -> Result<(), Box<dyn Error>> {
-    let temp_name = format!("./players/{}.csv", Uuid::new_v4());
+fn update_country_segment(segment: &str, country: &Country) -> Result<(), Box<dyn Error>> {
+    let temp_name = format!("./countries/{}.csv", Uuid::new_v4());
     {
         let mut source_reader = csv::Reader::from_path(&segment)?;
         let mut temp_writer = csv::Writer::from_path(&temp_name)?;
         let mut saved = false;
-        for p in source_reader.deserialize() {
-            let p: Player = p?;
+        for c in source_reader.deserialize() {
+            let c: Country = c?;
             if saved {
-                temp_writer.serialize(p)?;
+                temp_writer.serialize(c)?;
             } else {
-                match p.cmp(player) {
+                match c.cmp(country) {
                     Ordering::Greater => {
-                        temp_writer.serialize(player)?;
-                        temp_writer.serialize(p)?;
+                        temp_writer.serialize(country)?;
+                        temp_writer.serialize(c)?;
                         saved = true;
                     }
-                    Ordering::Less => temp_writer.serialize(p)?,
+                    Ordering::Less => temp_writer.serialize(c)?,
                     Ordering::Equal => {
-                        temp_writer.serialize(&p)?;
-                        match p.snooker_id.cmp(&player.snooker_id) {
-                            Ordering::Equal => saved = true,
-                            _ => saved = false,
-                        }
+                        temp_writer.serialize(&c)?;
+                        saved = false;
                     }
                 }
             }
         }
         if !saved {
-            temp_writer.serialize(player)?;
+            temp_writer.serialize(country)?;
         }
         temp_writer.flush()?;
     }
