358faa818467b48353deba574bf5bd29b156c462
Micah Teeuws <tutmirage@gmail.com>
2022-07-04 19:14:48 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -16,8 +16,8 @@ mod tables;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let mut rng = rand::thread_rng();
-    for _i in 1..5 {
-        let random_id: u32 = rng.gen_range(1..100);
+    for _i in 1..7 {
+        let random_id: u32 = rng.gen_range(1..300);
         let player = snooker::get_player(usize::try_from(random_id)?).await?;
         tables::add_player(&player)?;
         let country = Country::from(player.nation);
