0f6004726490434571873845d070d1b54d2bfee6
Micah Teeuws <tutmirage@gmail.com>
2022-07-05 11:21:15 +0300

    Update webclient.rs

--- src/services/webclient.rs
+++ src/services/webclient.rs
@@ -1,4 +1,4 @@
-use reqwest::Client
+use reqwest::Client;
 
 
 static APP_USER_AGENT : &str = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36 Edg/102.0.1245.33";
@@ -8,4 +8,13 @@ lazy_static! {
         .user_agent(APP_USER_AGENT)
         .build()
         .unwrap();
+}
+
+
+pub async fn get(url:String) -> Result<String, Box<dyn Error>> {
+    let resp = CLIENT
+        .get(url)
+        .send()
+        .await?;
+resp.text().await?
 }
