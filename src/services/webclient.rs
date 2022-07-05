cef9c329f2db19ee11b1994793eb31c545b919f9
Micah Teeuws <tutmirage@gmail.com>
2022-07-05 11:09:20 +0300

    Create webclient.rs

---
+++ src/services/webclient.rs
@@ -0,0 +1,11 @@
+use reqwest::Client
+
+
+static APP_USER_AGENT : &str = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36 Edg/102.0.1245.33";
+
+lazy_static! {
+    static ref CLIENT: Client = Client::builder()
+        .user_agent(APP_USER_AGENT)
+        .build()
+        .unwrap();
+}
