450a7223614dbff4c6f7275538c2efdf30decd1c
Micah Teeuws <tutmirage@gmail.com>
2022-07-07 22:58:10 +0300

    Update snooker.rs

--- src/snooker.rs
+++ src/snooker.rs
@@ -1,5 +1,5 @@
 use crate::services::web;
-use crate::services::web::html::{self, Link};
+use crate::services::web::html::{self, Link,Extract};
 use chrono::naive::MIN_DATE;
 use chrono::NaiveDate;
 use lazy_static::lazy_static;
