abbc531db639ae096ec9380e120f9f08c647637f
Micah Teeuws <tutmirage@gmail.com>
2022-07-15 10:55:01 +0300

    Create link.rs

---
+++ src/services/web/entities/link.rs
@@ -0,0 +1,40 @@
+#[derive(Debug)]
+pub struct Link {
+    pub url: String,
+    pub title: String,
+}
+
+impl Ord for Link {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.url.cmp(&other.url)
+    }
+}
+
+impl PartialOrd for Link {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl PartialEq for Link {
+    fn eq(&self, other: &Self) -> bool {
+        self.url == other.url
+    }
+}
+
+impl Eq for Link {}
+
+impl Hash for Link {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.url.hash(state);
+    }
+}
+
+impl<'a> From<ElementRef<'a>> for Link {
+    fn from(item: ElementRef<'a>) -> Self {
+        Link {
+            url: item.value().attr("href").unwrap_or("#").to_string(),
+            title: item.text().next().unwrap_or("").to_string(),
+        }
+    }
+}
