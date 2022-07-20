40f0fb9772776e558cf9e149a58a29e779b12114
Micah Teeuws <tutmirage@gmail.com>
2022-07-20 16:28:36 +0300

    Update update_readme.rs

--- src/app/tasks/update_readme.rs
+++ src/app/tasks/update_readme.rs
@@ -1,6 +1,8 @@
 use std::error::Error;
 use std::fs::{File, OpenOptions};
 
+static README_PATH     :     &str      = "./README.md";
+
 pub struct UpdateReadMe {}
 
 impl UpdateReadMe {
@@ -10,7 +12,7 @@ impl UpdateReadMe {
 
     fn execute(self) -> Result<(), Box<dyn Error>> {
         let shields = fs::read_to_string("./README/SHIELDS.md")?;
-        let mut file = OpenOptions::new().read(true).write(true).open()?;
+        let mut file = OpenOptions::new().read(true).write(true).open(README_PATH)?;
         file.write_all(shields.as_bytes())?;
         Ok(())
     }
