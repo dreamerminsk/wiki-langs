c54f28176b187d18f9e20d5194c0066fd5267d20
Micah Teeuws <tutmirage@gmail.com>
2022-07-20 11:49:50 +0300

    Update update_readme.rs

--- src/app/tasks/update_readme.rs
+++ src/app/tasks/update_readme.rs
@@ -1,7 +1,19 @@
+use std::error::Error;
+
+
+
 pub struct UpdateReadMe {}
 
 impl UpdateReadMe {
     fn new() -> Self {
         UpdateReadMe {}
     }
+
+
+
+
+fn execute(self)->Result<()  , Box <dyn     Error>>{
+
+Ok(())
+}
 }
