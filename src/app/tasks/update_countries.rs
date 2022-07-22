0a6ea5ccb8465f98421fed88875e43fa24e07409
Micah Teeuws <tutmirage@gmail.com>
2022-07-22 19:03:30 +0300

    Create update_countries.rs

---
+++ src/app/tasks/update_countries.rs
@@ -0,0 +1,18 @@
+
+
+pub struct UpdateCountries {}
+
+impl UpdateCountries {
+    pub fn new() -> Self {
+        UpdateCountries {}
+    }
+
+    pub fn execute(&self) -> Option<()> {
+        
+        Some(())
+    }
+
+ 
+
+   
+}
