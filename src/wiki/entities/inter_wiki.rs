43fbbe3147105359774ba110ca35709ca6e9ba14
Micah Teeuws <tutmirage@gmail.com>
2022-07-22 11:18:25 +0300

    Update inter_wiki.rs

--- src/wiki/entities/inter_wiki.rs
+++ src/wiki/entities/inter_wiki.rs
@@ -1,4 +1,13 @@
 pub struct InterWiki{
-pub lang:String;
-pub title:String;
+pub lang:String,
+pub title:String,
+}
+
+
+
+
+impl InterWiki{
+fn new(lang:String,title:String)->Self{
+InterWiki{lang,title,}
+}
 }
