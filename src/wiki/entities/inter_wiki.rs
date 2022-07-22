c166fe877e6dc430ffde0ac3241a71c93667abde
Micah Teeuws <tutmirage@gmail.com>
2022-07-22 18:44:10 +0300

    Update inter_wiki.rs

--- src/wiki/entities/inter_wiki.rs
+++ src/wiki/entities/inter_wiki.rs
@@ -1,3 +1,9 @@
+use std::fmt;
+
+
+
+
+
 #[derive(Debug)]
 pub struct InterWiki {
     pub lang: String,
@@ -7,5 +13,18 @@ pub struct InterWiki {
 impl InterWiki {
     fn new(lang: String, title: String) -> Self {
         InterWiki { lang, title }
+    }
+}
+
+
+
+
+
+
+
+
+impl fmt::Display for Position {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "({}, {})", self.longitude, self.latitude)
     }
 }
