26081e7da3bc8ab1d95cc4c28607a6864ea87c0f
Micah Teeuws <tutmirage@gmail.com>
2022-07-22 18:35:49 +0300

    Update mod.rs

--- src/wiki/mod.rs
+++ src/wiki/mod.rs
@@ -4,7 +4,7 @@ pub mod entities;
 
 pub async fn get_wiki(inter_wiki: InterWiki) -> Result<Page, Box<dyn Error>> {
     let page = web::get(inter_wiki.to_string()).await?;
-
+let page_title = "firstHeading";
     Ok(Page {
         lang: inter_wiki.lang,
         title: inter_wiki.title,
