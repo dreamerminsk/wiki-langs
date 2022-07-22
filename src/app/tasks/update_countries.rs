fe28ba62676ffcd6c1888f8efd051d91c4aacdc9
Micah Teeuws <tutmirage@gmail.com>
2022-07-22 19:30:49 +0300

    Update update_countries.rs

--- src/app/tasks/update_countries.rs
+++ src/app/tasks/update_countries.rs
@@ -1,5 +1,5 @@
+use crate::wiki::{self, InterWiki};
 use country::tables;
-use crate::wiki::{self,InterWiki};
 
 pub struct UpdateCountries {}
 
@@ -10,12 +10,11 @@ impl UpdateCountries {
 
     pub fn execute(&self) -> Option<()> {
         let list = tables::get_all_countries().ok()?;
-list.into_iter().filter(|c| c.wiki_id.is_none()).take(3)
-.filter_map(|c| wiki.get_wiki(InterWiki::new("en",c.name)).ok());
+        list.into_iter()
+            .filter(|c| c.wiki_id.is_none())
+            .take(3)
+            .filter_map(|c| wiki.get_wiki(InterWiki::new("en", c.name)).ok());
         Some(())
     }
-
- 
-
-   
 }
+
