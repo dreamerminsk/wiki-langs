b9457f0f9c12d8a78ca99b4ce070e3f36895c03f
Micah Teeuws <tutmirage@gmail.com>
2022-07-22 12:30:37 +0300

    Update mod.rs

--- src/wiki/mod.rs
+++ src/wiki/mod.rs
@@ -1,1 +1,13 @@
 pub mod entities;
+
+
+
+pub async fn get_wiki(inter_eiki: InterWiki) -> Result<Wiki, Box<dyn Error>> {
+    let page = web::get(format!("{}{}{}", urls::HOST, urls::PLAYER, snooker_id)).await?;
+
+  
+
+    Ok(Wiki {
+    
+    })
+}
