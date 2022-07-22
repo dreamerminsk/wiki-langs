472bbd59b324b1ec0070f9b1efdcfe5f2743ce53
Micah Teeuws <tutmirage@gmail.com>
2022-07-22 18:47:10 +0300

    Update inter_wiki.rs

--- src/wiki/entities/inter_wiki.rs
+++ src/wiki/entities/inter_wiki.rs
@@ -12,8 +12,8 @@ impl InterWiki {
     }
 }
 
-impl fmt::Display for Position {
+impl fmt::Display for      InterWiki    {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "({}, {})", self.longitude, self.latitude)
+        write!(f, "https://{}.wikipedia.org/wiki/", self.lang, self.title)
     }
 }
