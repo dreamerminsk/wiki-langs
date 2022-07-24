4d3336280d6c6827d635abb6e471b5c777b670c9
Micah Teeuws <tutmirage@gmail.com>
2022-07-24 22:21:10 +0300

    Update html.rs

--- src/app/web/html.rs
+++ src/app/web/html.rs
@@ -7,12 +7,12 @@ fn query(u: Url) -> HashMap<String, String> {
     u.query_pairs().into_owned().collect()
 }
 
-pub trait Extract {
+pub trait HtmlExt {
     fn extract_links(&self) -> BTreeSet<Link>;
     fn extract_text(&self, selector: &str) -> Option<String>;
 }
 
-impl Extract for Html {
+impl HtmlExt for Html {
     fn extract_links(&self) -> BTreeSet<Link> {
         let selector = Selector::parse(r#"a"#).unwrap();
         self.select(&selector)
