cfc1e4858c30fd4dba99ab836311a7d22b894310
Micah Teeuws <tutmirage@gmail.com>
2022-07-25 20:46:12 +0300

    Update inter_wiki.rs

--- src/wiki/entities/inter_wiki.rs
+++ src/wiki/entities/inter_wiki.rs
@@ -1,7 +1,7 @@
 use std::{
     cmp::{Eq, Ord, PartialEq, PartialOrd},
     fmt,
-    hash::Hash,
+    hash::Hash,convert::From,
 };
 
 #[derive(Debug, Ord, PartialOrd, Eq, PartialEq, Hash)]
@@ -20,4 +20,19 @@ impl fmt::Display for InterWiki {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "https://{}.wikipedia.org/wiki/{}", self.lang, self.title)
     }
+}
+
+
+
+
+
+impl<'a> From<ElementRef<'a>> for InterWiki {
+    fn from(item: ElementRef<'a>) -> Self {
+        InterWiki {
+            lang: item.value().attr("lang").unwrap_or_default().to_string(),
+            title: extract_title(item.value().attr("title").unwrap_or_default()),
+        }
+    }
+fn extract_title(text:&str)->String{
+}
 }
