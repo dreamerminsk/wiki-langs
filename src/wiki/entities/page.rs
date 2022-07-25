465705b4ada4fb330e105fd6ddf23b4ac22771d6
Micah Teeuws <tutmirage@gmail.com>
2022-07-25 18:10:13 +0300

    Update page.rs

--- src/wiki/entities/page.rs
+++ src/wiki/entities/page.rs
@@ -1,16 +1,22 @@
+use std::collections::BTreeSet;
+use crate::wiki::entities::InterWiki;
+
+
 #[derive(Debug)]
 pub struct Page {
     pub lang: String,
     pub title: String,
     pub wikidata: Option<String>,
+pub inter_wikis :   BTreeSet<InterWiki>,
 }
 
 impl Page {
-    fn new(lang: String, title: String, wikidata: Option<String>) -> Self {
+    fn new(lang: String, title: String, wikidata: Option<String>,inter_wikis:BtreeSet<InterWiki>) -> Self {
         Page {
             lang,
             title,
             wikidata,
+inter_wikis,
         }
     }
 }
