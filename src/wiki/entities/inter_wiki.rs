5e97ff9398e118b4ba57d592d39712eb28933048
Micah Teeuws <tutmirage@gmail.com>
2022-07-25 21:02:01 +0300

    Update inter_wiki.rs

--- src/wiki/entities/inter_wiki.rs
+++ src/wiki/entities/inter_wiki.rs
@@ -34,12 +34,17 @@ impl<'a> From<ElementRef<'a>> for InterWiki {
                 .unwrap_or_default(),
         }
     }
-    fn extract_title(text: &str) -> Option<String> {
+    
+}
+
+
+
+
+fn extract_title(text: &str) -> Option<String> {
         lazy_static! {
             static ref TITLE_RE: Regex = Regex::new(r"(?P<title>.*?) - .*?").unwrap();
         }
         TITLE_RE
-            .captures(input)
+            .captures(text)
             .and_then(|cap| cap.name("title").map(|title| title.as_str().to_string()))
     }
-}
