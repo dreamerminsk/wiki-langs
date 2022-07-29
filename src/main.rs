1bb64832a3b8829429ea20db4d1919887831fdfe
Micah Teeuws <tutmirage@gmail.com>
2022-07-29 10:33:15 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ mod wiki;
 struct NextPlayer(usize);
 impl NextPlayer {
     fn get(&self) -> usize {
-        self.0 = read_to_string("./next-player.csv")
+        self.0 = read_to_string("./next-player.csv").ok()
             .and_then(|t| t.parse::<usize>().ok())
             .unwrap_or_default();
         self.0
