b63767506b3de1697ad2ad84c081d020d5eb93ee
Micah Teeuws <tutmirage@gmail.com>
2022-07-29 21:57:40 +0300

    Create event.rs

---
+++ src/snooker/entities/event.rs
@@ -0,0 +1,40 @@
+use chrono::{naive::MIN_DATE, NaiveDate};
+use serde::{Deserialize, Serialize};
+use std::{
+    cmp::Ordering,
+    hash::{Hash, Hasher},
+};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Event {
+    
+    pub snooker_id: usize,
+pub title: String,
+}
+
+impl Ord for Event {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.snooker_id
+            .cmp(&other.snooker_id)
+    }
+}
+
+impl PartialOrd for Event {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl PartialEq for Event {
+    fn eq(&self, other: &Self) -> bool {
+        self.snooker_id == other.snooker_id
+    }
+}
+
+impl Eq for Event {}
+
+impl Hash for Event {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.snooker_id.hash(state);
+    }
+}
