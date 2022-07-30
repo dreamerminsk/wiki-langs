72f349ec6cbd509aba5004ef37908816497acec8
Micah Teeuws <tutmirage@gmail.com>
2022-07-30 21:50:52 +0300

    Update update_countries.rs

--- src/app/tasks/update_countries.rs
+++ src/app/tasks/update_countries.rs
@@ -20,15 +20,16 @@ impl UpdateCountries {
             .take(5);
         for c in filtered {
             let updated = self.get_wiki(c.name.as_str()).await;
-            let iw = updated
+            let oiw = updated
                 .inter_wikis
                 .into_iter()
                 .filter(|u| u.lang == "uk")
                 .next();
-            if iw.is_some() {
-                updated.name = iw.title.clone();
-                updated.wiki_id = Some(iw.title.clone());
-                tables::add_country("uk", &updated);
+            if oiw.is_some() {
+let iw   = oiw.unwrap();
+                c.name = iw.title.clone();
+                c.wiki_id = Some(iw.title.clone());
+                tables::add_country("uk", &c);
             }
         }
         Some(())
