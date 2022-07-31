769c8085cc763914de336f25f20cb87e78659e47
Micah Teeuws <tutmirage@gmail.com>
2022-07-31 23:42:33 +0300

    Update segments.rs

--- src/players/tables/segments.rs
+++ src/players/tables/segments.rs
@@ -5,7 +5,7 @@ pub struct Segments {
 }
 
 impl Segments {
-    pub fn new<S: AsRef<OsStr>>(path: S) -> Segments {
+    pub fn new(path: String) -> Segments {
         Segments {
             root: PathBuf::from(path),
         }
