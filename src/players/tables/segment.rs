6ff73cc5cd54bbd2a19194acdb9059e0ec7ef4d3
Micah Teeuws <tutmirage@gmail.com>
2022-08-01 18:32:07 +0300

    Update segment.rs

--- src/players/tables/segment.rs
+++ src/players/tables/segment.rs
@@ -1,16 +1,17 @@
 use csv;
 use std::{io, path::Path};
+use crate::snooker::entities::Player;
 
 pub struct Segment {
-    items: Player,
+    items: Vec<Player>,
 }
 
 impl Segment {
     pub fn open<P: AsRef<Path>>(path: P) -> io::Result<Segment> {
         let mut items = vec![];
-        if Path::new(&path).exists() {
-            let mut source_reader = csv::Reader::from_path(&path)?;
-            for c in source_reader.deserialize() {
+        if Path::new(path).exists() {
+            let mut source_reader = csv::Reader::from_path(path)?;
+            for p in source_reader.deserialize() {
                 let p: Player = p?;
                 items.push(&p);
             }
