4d8df41e131d81144a5bac807d2bf32216ab1c64
Micah Teeuws <tutmirage@gmail.com>
2022-08-02 06:56:04 +0300

    Update update_readme.rs

--- src/app/tasks/update_readme.rs
+++ src/app/tasks/update_readme.rs
@@ -39,7 +39,7 @@ impl UpdateReadMe {
         format!(
             "{}\r\n{}\r\n",
             self.shields().unwrap_or_else(|| "".to_string()),
-            self.players2().unwrap_or_else(|| "".to_string())
+            self.players().unwrap_or_else(|| "".to_string())
         )
     }
 
@@ -47,34 +47,10 @@ impl UpdateReadMe {
         fs::read_to_string(SHIELDS_PATH).ok()
     }
 
+    
+
+
     fn players(&self) -> Option<String> {
-        let files = fs::read_dir("./players").ok()?;
-        let mut rows: Vec<String> = files
-            .into_iter()
-            .filter_map(|di| di.ok())
-            .map(|di| {
-                format!(
-                    "| {}s | [{}](players/{}) |",
-                    self.osstr_to_year(di.file_name().as_os_str()),
-                    di.metadata().unwrap().len(),
-                    di.file_name().into_string().unwrap()
-                )
-            })
-            .collect();
-        rows.sort();
-        Some(format!(
-            "## players\r\n<sup>last modified: {}</sup>\r\n{}\r\n{}\r\n",
-            Utc::now().to_rfc2822(),
-            PLAYERS_HEADER,
-            rows.join("\r\n")
-        ))
-    }
-
-    fn osstr_to_year(&self, r#fn: &OsStr) -> String {
-        r#fn.to_str().unwrap_or("0000").chars().take(4).collect()
-    }
-
-    fn players2(&self) -> Option<String> {
         let segs = Segments::open("./players").ok()?;
         let mut years = BTreeMap::<String, usize>::new();
         segs.into_iter().flat_map(|s| s.into_iter()).for_each(|p| {
