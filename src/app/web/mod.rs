56d9a267dcec489f86b0352cfd0f7d78a358b7b5
Micah Teeuws <tutmirage@gmail.com>
2022-08-17 13:07:03 +0300

    Update mod.rs

--- src/app/web/mod.rs
+++ src/app/web/mod.rs
@@ -23,8 +23,8 @@ pub async fn get(url: String) -> Result<Html, Box<dyn Error>> {
     info!(
         "get('{}') -> {},{}",
         url,
-        resp.status().as_str(),
-        resp.status().canonical_reason()
+        resp.status().as_str().unwrap(),
+        resp.status().canonical_reason().unwrap()
     );
     let content = resp.text().await?;
     Ok(Html::parse_document(&content))
