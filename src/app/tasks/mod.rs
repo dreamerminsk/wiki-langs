95a40db54978f7ba38b7398684e75d144c6a1810
Micah Teeuws <tutmirage@gmail.com>
2025-10-26 22:43:15 +0300

    Update mod.rs

--- src/app/tasks/mod.rs
+++ src/app/tasks/mod.rs
@@ -1,5 +1,7 @@
 pub use crate::app::tasks::update_countries::UpdateCountries;
 pub use crate::app::tasks::update_readme::UpdateReadMe;
+pub use crate::app::tasks::nation_stats::NationStats;
 
 mod update_countries;
 mod update_readme;
+mod nation_stats;
