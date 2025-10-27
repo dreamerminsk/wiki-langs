89682fdc5b4baa13b3135852d06e2910f501611e
Micah Teeuws <tutmirage@gmail.com>
2025-10-27 16:11:03 +0300

    Update nation_stats.rs

--- src/app/tasks/nation_stats.rs
+++ src/app/tasks/nation_stats.rs
@@ -39,53 +39,54 @@ impl NationStats {
 
     fn nations(&self) -> Option<String> {
         let segs = Segments::open("./players").ok()?;
-        let mut counts: BTreeMap<String, usize> = BTreeMap::new();
-        let mut decades = BTreeMap::new();
+        let mut nation_counts: BTreeMap<String, usize> = BTreeMap::new();
+    let mut decade_counts: BTreeMap<(String, i32), usize> = BTreeMap::new();
         let mut min_decade = 3000;
         let mut max_decade = 1000;
 
-        segs.into_iter().flat_map(|s| s.into_iter()).for_each(|p| {
-            *counts.entry(p.nation).or_insert(0) += 1;
-            if p.birthday.is_some() {
-                let bd = p.birthday.unwrap();
-                let decade = bd.year() - bd.year() % 10;
-                if decade < min_decade {
-                    min_decade = decade;
-                }
-                if decade > max_decade {
-                    max_decade = decade;
-                }
-                *decades.entry((p.nation, decade)).or_insert(0) += 1;
+        segs.into_iter().flat_map(|s| s.into_iter()).for_each(|player| {
+            *nation_counts.entry(player.nation.clone()).or_insert(0) += 1;
+if let Some(birthday) = player.birthday {
+            let decade = (birthday.year() / 10) * 10;
+
+            if decade < min_decade {
+                min_decade = decade;
             }
-        });
+            if decade > max_decade {
+                max_decade = decade;
+            }
+            *decade_counts.entry((player.nation.clone(), decade)).or_insert(0) += 1;
+        }
+});
+            
 
         let mut header = String::from("| Nation ");
-        for decade in (min_decade..=max_decade).step_by(10) {
-            header.push_str(&format!("| {} ", decade));
-        }
-        header.push_str("| Total |\n");
+    for decade in (min_decade..=max_decade).step_by(10) {
+        header.push_str(&format!("| {} ", decade));
+    }
+    header.push_str("| Total |\n");
 
         let mut separator = String::from("|:-------");
-        for _ in (min_decade..=max_decade).step_by(10) {
-            separator.push_str("|:-------:");
+    for _ in (min_decade..=max_decade).step_by(10) {
+        separator.push_str("|:-------:");
+    }
+    separator.push_str("|:-------------:|\n");
+
+    let mut table = header;
+    table.push_str(&separator);
+
+        let mut sorted_by_value: Vec<(&String, usize)> = nation_counts.iter().collect();
+    sorted_by_value.sort_by(|a, b| b.1.cmp(&a.1));
+
+        for (nation, total_count) in sorted_by_value.iter() {
+        let mut row = format!("| {} ", nation);
+        for decade in (min_decade..=max_decade).step_by(10) {
+            let count = decade_counts.get(&(nation.clone(), decade)).unwrap_or(&0); // Use clone to avoid ownership issues
+            row.push_str(&format!("| {} ", count));
         }
-        separator.push_str("|:-------------:|\n");
-
-        let mut table = header;
-        table.push_str(&separator);
-
-        let mut sorted_by_value: Vec<(&str, usize)> = map.into_iter().collect();
-        sorted_by_value.sort_by(|a, b| b.1.cmp(&a.1));
-
-        for nation in sorted_by_value.iter() {
-            let mut row = format!("| {}s ", nation);
-            for decade in (min_decade..=max_decade).step_by(10) {
-                let count = decades.get((nation, decade)).unwrap_or(&0);
-                row.push_str(&format!("| {} ", count));
-            }
-            row.push_str(&format!("| {} |\n", nation 1));
-            table.push_str(&row);
-        }
+        row.push_str(&format!("| {} |\n", total_count));
+        table.push_str(&row);
+    }
 
         Some(format!("## players\r\n{}\r\n", table,))
     }
