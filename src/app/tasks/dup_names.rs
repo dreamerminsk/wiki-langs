83ba0b56d56bb1c5c3c19e0de46ab3a176fa1e4d
Micah Teeuws <tutmirage@gmail.com>
2025-10-29 18:47:47 +0300

    Update dup_names.rs

--- src/app/tasks/dup_names.rs
+++ src/app/tasks/dup_names.rs
@@ -35,7 +35,7 @@ impl DupNames {
 
     fn players(&self) -> Option<String> {
         let segs = Segments::open("./players").ok()?;
-        let mut name_map: BTreeMap<String, Vec<Player>> = HashMap::new();
+        let mut name_map: BTreeMap<String, Vec<Player>> = BTreeMap::new();
 
         for player in segs.into_iter().flat_map(|s| s.into_iter()) {
             name_map
