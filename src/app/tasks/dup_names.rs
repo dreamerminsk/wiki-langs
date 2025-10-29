b4a67ef414d7be9c7400ad21c60fdb2a302222a9
Micah Teeuws <tutmirage@gmail.com>
2025-10-29 15:07:23 +0300

    Update dup_names.rs

--- src/app/tasks/dup_names.rs
+++ src/app/tasks/dup_names.rs
@@ -1,10 +1,7 @@
 use crate::{players::tables::Segments, snooker::entities::Player};
-use chrono::{Datelike, Utc};
-use std::{
-    collections::{BTreeMap, BTreeSet, HashMap},
-    fs::{self, OpenOptions},
-    io::Write,
-};
+use std::collections::HashMap;
+use std::fs::{self, OpenOptions};
+use std::io::Write;
 
 static DUP_PATH: &str = "./REPORTS/DUP–NAMES.md";
 
@@ -17,30 +14,37 @@ impl DupNames {
 
     pub fn execute(&self) -> Option<()> {
         let mut file = OpenOptions::new()
+            .create(true)
             .read(true)
             .write(true)
             .truncate(true)
             .open(DUP_PATH)
             .ok()?;
 
-        let content = format!("{}", self.content());
+        let content = self.content().unwrap_or_else(|| String::from(""));
 
         file.write_all(content.as_bytes()).ok()?;
         file.flush().ok()?;
         Some(())
     }
 
-    fn content(&self) -> String {
-        format!("{}\r\n", self.players().unwrap_or_else(|| "".to_string()),)
+    fn content(&self) -> Option<String> {
+        let players_info = self.players()?;
+        Some(format!("{}\r\n", players_info))
     }
 
     fn players(&self) -> Option<String> {
         let segs = Segments::open("./players").ok()?;
         let mut name_map: HashMap<String, Vec<Player>> = HashMap::new();
 
-        segs.into_iter().flat_map(|s| s.into_iter()).for_each(|p| {
-            name_map.entry(p.name.clone()).or_default().push(p);
-        });
+        for player in segs.into_iter().flat_map(|s| s.into_iter()) {
+            name_map.entry(player.name.clone()).or_default().push(player);
+        }
+
+        let mut table = String::new();
+        
+        table.push_str("| Name | Count |\n");
+        table.push_str("|------|-------|\n"); // Добавляем разделитель
 
         for (name, group) in name_map {
             if group.len() > 1 {
@@ -49,6 +53,6 @@ impl DupNames {
             }
         }
 
-        Some(format!("{}\r\n", table,))
+        Some(table)
     }
 }
