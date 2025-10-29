928193b412aa38f065b3b7d18e5c4413450f3b3d
Micah Teeuws <tutmirage@gmail.com>
2025-10-29 20:10:49 +0300

    Update src/app/tasks/mod.rs
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

--- src/app/tasks/mod.rs
+++ src/app/tasks/mod.rs
@@ -1,4 +1,4 @@
-pub use crate::app::tasks::dup_burthdays::DupBirthdays;
+pub use crate::app::tasks::dup_birthdays::DupBirthdays;
 pub use crate::app::tasks::dup_names::DupNames;
 pub use crate::app::tasks::nation_stats::NationStats;
 pub use crate::app::tasks::so_ranking::SoRanking;
