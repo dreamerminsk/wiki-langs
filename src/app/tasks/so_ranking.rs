49d42c071604503987e5f9d04fa839529010eaa9
Micah Teeuws <tutmirage@gmail.com>
2025-11-02 20:27:59 +0300

    Update so_ranking.rs

--- src/app/tasks/so_ranking.rs
+++ src/app/tasks/so_ranking.rs
@@ -7,7 +7,7 @@ use std::fs::File;
 use std::io::{BufWriter, Write};
 use std::time::Duration;
 
-const APP_USER_AGENT: &str = "Your User Agent Here";
+const APP_USER_AGENT: &str = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36 Edg/102.0.1245.33";
 const NATIONALITY_REPORT_PATH: &str = "./nationality_report.txt";
 
 pub struct SoRanking {
