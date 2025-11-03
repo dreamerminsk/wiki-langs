1b03def531fb9fe27625897a6f34dfc8f188cc63
Micah Teeuws <tutmirage@gmail.com>
2025-11-03 21:24:56 +0300

    Update segments.rs

--- src/players/tables/segments.rs
+++ src/players/tables/segments.rs
@@ -12,7 +12,7 @@ pub struct Segments {
 
 impl Segments {
     pub fn open<P: AsRef<Path>>(path: P) -> io::Result<Segments> {
-        let mut entries = fs::read_dir(path)?
+        let entries = fs::read_dir(path)?
             .map(|res| res.and_then(|e| Segment::open(e.path())))
             .collect::<Result<Vec<_>, io::Error>>()?;
         Ok(Segments { entries })
