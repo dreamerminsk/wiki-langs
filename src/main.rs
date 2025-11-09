1e6eb0d15d37b3f7a8e496f55bfd1aa9ae68c498
Micah Teeuws <tutmirage@gmail.com>
2025-11-09 16:22:18 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -58,7 +58,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     match get_file_last_modified(README_PATH) {
         Ok(timediff) => {
             info!("README.md timediff: {}", timediff);
-            if timediff >= 86400 {
+            if timediff < 86400 {
                 let update_readme = UpdateReadMe::new();
                 update_readme.execute();
             }
